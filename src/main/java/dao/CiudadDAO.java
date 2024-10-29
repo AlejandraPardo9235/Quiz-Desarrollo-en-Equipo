99f828906c4f36a89e6b4fa96c4ae9cf13cdf0ce
nicolas00977 <88044373+nicolas00977@users.noreply.github.com>
2024-10-28 20:00:57 -0500

    Desarrollo 3
    Desarrollo 3

--- src/main/java/dao/CiudadDAO.java
+++ src/main/java/dao/CiudadDAO.java
@@ -1,16 +1,15 @@
-
 import entidades.Ciudad;
-import java.util.List;
+import javax.persistence.EntityManager;
 
-public class CiudadDAO extends GenericDAO<Ciudad> {
-    private static final String FILE_PATH = "src/main/resources/ciudades.csv";
+public class CiudadDoo extends GenericDAO<Ciudad> {
+    private static final String[] CSV_FILE_PATHS = { "src/main/resources/ciudades.csv" };
 
-    public CiudadDAO() {
-        super(Ciudad.class);
+    public CiudadDoo(EntityManager entityManager) {
+        super(Ciudad.class, entityManager);
     }
 
-    public List<Ciudad> getAllCiudades() {
-        return getAll(FILE_PATH);
+    public void cargarCiudades() {
+        // Invoca el método getAll para cargar todas las ciudades desde los archivos CSV en una única ejecución
+        getAll(CSV_FILE_PATHS);
     }
 }
-
